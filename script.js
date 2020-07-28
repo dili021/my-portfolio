e08b2c5d72522ed2fe24f79673acc90fba05b391
stefan dili <stefan.dili.021@gmail.com>
2020-07-28 12:44:18 +0200

    finished sidebar and form validation

--- script.js
+++ script.js
@@ -3,36 +3,29 @@ const name = form.querySelector("#name");
 const email = form.querySelector("#email");
 const text = form.querySelector("#text");
 const menuBtn = document.querySelector("#menu-btn");
-const mobMenu = document.querySelector(".m-nav");
+const mobMenu = document.querySelector(".sidebar");
 const nav = document.querySelector("nav");
 
-function changeMenuIcon() {
-  menuBtn.innerHTML = `
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-    >
-      <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
-    </svg>
-    `;
+function openMenu() {
+  mobMenu.style.width = "250px";
+  document.querySelector(".m-wrap").style.marginLeft = "250px";
 }
 
-function showMenu() {
-  nav.insertAdjacentElement("afterend", mobMenu);
-  mobMenu.classList.toggle("open");
+function closeMenu() {
+  mobMenu.style.width = "0";
+  document.querySelector(".m-wrap").style.marginLeft = "0";
 }
 
 function showError(input, msg) {
   const formControl = input.parentElement;
-  formControl.classList.add("error");
+  input.classList.add("border-2", "border-red-700");
+  formControl.classList.add("text-red-700");
   const errorEl = formControl.querySelector("small");
   errorEl.innerText = msg;
 }
 
 function showSuccess(input) {
-  input.parentElement.classList.add("success");
+  input.classList.add("border-2", "border-green-500");
 }
 
 function getFieldName(input) {
@@ -43,8 +36,9 @@ function validateInput(inputArray) {
   inputArray.forEach((input) => {
     if (input.value.trim() === "") {
       showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
     }
-    showSuccess(input);
   });
 }
 
@@ -67,11 +61,10 @@ function validateEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (!re.test(email.value)) {
     showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
   }
-  showSuccess(email);
 }
-
-menuBtn.addEventListener("click", showMenu);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -80,3 +73,11 @@ form.addEventListener("submit", (e) => {
   validateLength(text, 20, 1000);
   validateEmail(email);
 });
+
+function suggestRefresh() {
+  if (window.innerWidth <= 768 && document.location === "index.html") {
+    alert("refresh your browser to load the mobile version");
+  }
+}
+
+onresize = suggestRefresh;
