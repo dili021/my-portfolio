27bf7a0641061a3bce20add8b4132ac545d49c4b
stefan dili <stefan.dili.021@gmail.com>
2020-07-29 10:26:09 +0200

    fixed device dependant loading issues, added tailwind config, added article links

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ form.addEventListener("submit", (e) => {
 });
 
 function suggestRefresh() {
-  if (window.innerWidth <= 768 && document.location === "index.html") {
+  if (innerWidth <= 768 && document.location == "index.html") {
     alert("refresh your browser to load the mobile version");
   }
 }
